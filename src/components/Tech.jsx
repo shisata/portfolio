b0394f82225241d6702000ca605334038486db11
shisata <joshvalve@gmail.com>
2026-03-24 00:51:04 +0700

    adjust logo sizing (different screen size)

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -16,7 +16,7 @@ export const Tech = ({name, logo, position, highlight, ...props}) => {
     return (
         <div id="tech-container" className={`rounded-4xl w-[95vw] md:w-[50vw] h-[12vh] gap-x-[2vw] mt-[1rem] place-items-center justify-center md:justify-center flex transition ease-in duration-70 ${inView? highlight : ''} ${position}`}>
             <h2 className={`text-5xl font-medium`} ref={ref}>{name}</h2> 
-            <img src={logo} alt="" className={`w-[5vw] h-[5vw] saturate-0 ${inView? `saturate-100` : ``}`}/>
+            <img src={logo} alt="" className={`w-[2.5rem] h-[2.5rem] lg:w-[5rem] lg:h-[5rem] saturate-0 ${inView? `saturate-100` : ``}`}/>
         </div>
     )
 }
