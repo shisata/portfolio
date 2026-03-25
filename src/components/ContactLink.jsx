96c4c5479f888bdcacc7afe9a455a001fb4fbbdc
shisata <joshvalve@gmail.com>
2026-03-26 02:50:46 +0700

    adjusted contact links open new tab

--- src/components/ContactLink.jsx
+++ src/components/ContactLink.jsx
@@ -18,7 +18,7 @@ const logoColors = {
 export const ContactLink = ({name, logoName, logoColor = "white", url = ""}) => {
     const emailHighlight = `${(logoName == "email")? "underline decoration-accent3-muted group-hover:decoration-accent3 group-hover:bg-accent3 group-hover:text-text-default" : ""}`
     return (
-        <a href={url} className="flex gap-x-2 items-center group">
+        <a href={url} target="_blank" rel="noopener noreferrer" className="flex gap-x-2 items-center group">
             <img src={logos[logoName]} alt="" className={`w-[1.5rem] h-[1.5rem] p-[0.15rem] rounded-full ${logoColors[logoColor]}`}/>
             <h2 className={`${emailHighlight} text-text-default group-hover:text-accent3 duration-200 transition-all text-[1rem] font-semibold flex gap-2 mb-0`}>{name}</h2>
         </a>
