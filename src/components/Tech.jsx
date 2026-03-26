dc7336eecb33b625f2155afb2e0ec20ac788c4ce
shisata <joshvalve@gmail.com>
2026-03-26 21:58:30 +0700

    added hover effects for slogan

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -17,7 +17,7 @@ export const Tech = ({name, logo, position, highlight, ...props}) => {
     const expressLogoStyling = `${(name == "Express") ? "bg-logo-express" : ""}`
 
     return (
-        <div id="tech-container" className={`w-[70vw] md:w-[50vw] h-[12vh] gap-x-[2vw] mt-[1rem] place-items-center justify-center flex transition ease-in duration-70 ${inView? highlight : 'text-text-muted'} ${position}`}>
+        <div id="tech-container" className={`w-[70vw] md:w-[50vw] h-[12vh] gap-x-[2vw] mt-[1rem] place-items-center justify-center flex transition ease-in duration-30 ${inView? highlight : 'text-text-muted'} ${position}`}>
             <h2 className={`text-3xl lg:text-5xl :font-medium`} ref={ref}>{name}</h2> 
             <img src={logo} alt="" className={`w-[2.5rem] h-[2.5rem] lg:w-[5rem] lg:h-[5rem] saturate-0  ${inView? `saturate-100` : ``} ${expressLogoStyling}`}/>
         </div>
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,7 @@
 import { AboutSummary } from "@/layout/AboutSummary"
 import { UnderConstruction } from "@/components/UnderConstruction"
 import portraitNoBackgroundUrl from "/src/images/portrait-no-background.png"
+import { checkUserIsMobile } from "@/lib/checkUserIsMobile"
 
 const h = (text) => {
   return <span className="bg-accent2 text-background-main">{text}</span>
@@ -12,23 +13,25 @@ const textbox = () => {
   </p>
 }
 
+const pictureDefaultColor = checkUserIsMobile() ? `grayscale-0` : `grayscale`
+const transitionTime = `transiton-all duration-400`
 const heroItemCentering = `absolute mx-auto left-0 right-0`
-const commonSloganStyle = `${heroItemCentering} bottom-auto font-black text-center text-7xl sm:text-9xl`
+const commonSloganStyle = `${heroItemCentering} bottom-auto font-black text-center text-7xl sm:text-9xl group-hover:tracking-widest group-hover:translate-y-[-5vh] ${transitionTime}`
 
 export const About = () => {
     return(
       <main className={`px-[5%] min-h-screen`}>
         <div name="heo-section" className={`w-full h-screen flex flex-col items-center`}>
           {/* Container responsible for image and slogan */}
-          <div name="slogan-container" className={`w-full lg:w-[60%] h-[50%] lg:h-[70%] relative`}> 
+          <div name="slogan-container" className={`w-full lg:w-[60%] h-[50%] lg:h-[70%] relative group`}> 
             <h1 className={`${commonSloganStyle} z-1 top-[30%] sm:top-[20%] xl:top-[40%]`}>ABOUT</h1>
             <h1 className={`${commonSloganStyle} z-3 top-[60%] sm:top-[50%] xl:top-[55%]`}>ME</h1>
-            <div className={`${heroItemCentering} z-2 top-auto bottom-0 w-full md:w-[60%]`}>
-              <img src={portraitNoBackgroundUrl} alt={``} className={`grayscale`}/>
+            <div className={`${heroItemCentering} z-2 top-auto bottom-0 w-full md:w-[60%] ${transitionTime} group-hover:translate-y-[10vh] group-hover:scale-90`}>
+              <img src={portraitNoBackgroundUrl} alt={``} className={`${pictureDefaultColor} group-hover:grayscale-0`}/>
             </div>
             {/* Message box below slogan */}
             <div className={`${heroItemCentering} z-3 top-[95%] w-full min-h-[20rem] h-auto rounded-4xl border-[0.75px] border-text-extra-muted bg-background-lighter flex flex-col items-center justify-center p-[1rem]`}>
-              <p>{textbox()}</p>
+              {textbox()}
             </div>
           </div>
         </div>
