02f329dfd9a47531587924aa5f753da2f6e9cf52
shisata <joshvalve@gmail.com>
2026-04-19 22:15:46 +0700

    update resume url

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -12,7 +12,7 @@ const links = [
     { name: "Contact",to: "/contact" },
 ]
 
-const resumeLink = `https://drive.google.com/file/d/1mE5rIBnrGi2-vZW0_tQ_BFfV1QrByC4A/view`
+const resumeLink = `https://drive.google.com/file/d/16Vliaeab3A_rTyaSND2XaPApMU1v62GK/view`
 
 export const Navbar = () => {
     const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
